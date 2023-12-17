3fa79c6d196cd90e5562aeb667b663bee08fa278
AwanWicak1 <143805465+AwanWicak1@users.noreply.github.com>
2023-12-17 23:57:46 +0700

    Update fitur login, register, dll

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-from flask import Flask, redirect, url_for, render_template, request
 from pymongo import MongoClient
+import jwt
+from datetime import datetime, timedelta
+import hashlib
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect,
+    url_for,
+)
+import json
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["UPLOAD_FOLDER"] = "./static/profile_pics"
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -12,7 +29,9 @@ DB_NAME = os.environ.get("DB_NAME")
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
 
-app = Flask(__name__)
+SECRET_KEY = "SPARTA"
+
+TOKEN_KEY = 'mytoken'
 
 
 @app.route('/daftaradmin')
@@ -40,11 +59,6 @@ def homevisitor():
     return render_template('homevisitor.html')
 
 
-@app.route('/')
-def login():
-    return render_template('index.html')
-
-
 @app.route('/keranjang')
 def keranjang():
     return render_template('keranjang.html')
@@ -68,6 +82,120 @@ def register():
 @app.route('/tambahbuku')
 def tambahbuku():
     return render_template('tambahbuku.html')
+
+
+@app.route("/")
+def home():
+    token_receive = request.cookies.get(TOKEN_KEY)
+    try:
+        payload = jwt.decode(
+            token_receive,
+            SECRET_KEY,
+            algorithms=["HS256"]
+        )
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('homevisitor.html', user_info=user_info)
+
+    except jwt.ExpiredSignatureError:
+        msg = 'Your token has expired!'
+        return redirect(url_for("login", msg=msg))
+    except jwt.exceptions.DecodeError:
+        msg = 'There was a problem logging you in!'
+        return redirect(url_for("login", msg=msg))
+
+
+@app.route("/login")
+def login():
+    msg = request.args.get("msg")
+    return render_template("index.html", msg=msg)
+
+
+@app.route("/user/<username>")
+def user(username):
+    token_receive = request.cookies.get(TOKEN_KEY)
+    try:
+        payload = jwt.decode(
+            token_receive,
+            SECRET_KEY,
+            algorithms=["HS256"]
+        )
+        status = username == payload["id"]
+        user_info = db.users.find_one({
+            "username": username},
+            {"_id": False}
+        )
+        return render_template(
+            "user.html",
+            user_info=user_info,
+            status=status
+        )
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+@app.route("/sign_in", methods=["POST"])
+def sign_in():
+    # Sign in
+    username_receive = request.form["username_give"]
+    password_receive = request.form["password_give"]
+    pw_hash = hashlib.sha256(password_receive.encode("utf-8")).hexdigest()
+    result = db.users.find_one(
+        {
+            "username": username_receive,
+            "password": pw_hash,
+        }
+    )
+    if result:
+        payload = {
+            "id": username_receive,
+            # the token will be valid for 24 hours
+            "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 24),
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+        return jsonify(
+            {
+                "result": "success",
+                "token": token,
+            }
+        )
+    # Let's also handle the case where the id and
+    # password combination cannot be found
+    else:
+        return jsonify(
+            {
+                "result": "fail",
+                "msg": "We could not find a user with that id/password combination",
+            }
+        )
+
+
+@app.route("/sign_up/save", methods=["POST"])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(
+        password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # id
+        "password": password_hash,                                  # password
+        # user's name is set to their id by default
+        "profile_name": username_receive,
+        # profile image file name
+        "profile_pic": "",
+        "profile_pic_real": "profile_pics/profile_placeholder.png",  # a default profile image
+        # a profile description
+        "profile_info": ""
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 
 if __name__ == '__main__':
